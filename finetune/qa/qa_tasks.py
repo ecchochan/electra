bf57587c01814ebefccb2fccae4b92be502b900f
ecchochan <ecchochan@gmail.com>
2020-08-28 15:01:19 +0800

    update

--- finetune/qa/qa_tasks.py
+++ finetune/qa/qa_tasks.py
@@ -141,11 +141,11 @@ def _improve_answer_span(doc_tokens, input_start, input_end, tokenizer,
   # the word "Japanese". Since our WordPiece tokenizer does not split
   # "Japanese", we just use "Japanese" as the annotation. This is fairly rare
   # in SQuAD, but does happen.
-  tok_answer_text = " ".join(tokenizer.encode(orig_answer_text).tokens)
+  tok_answer_text = tokenizer.encode(orig_answer_text).ids
 
   for new_start in range(input_start, input_end + 1):
     for new_end in range(input_end, new_start - 1, -1):
-      text_span = " ".join(tokenizer.id_to_token(x) for x in doc_tokens[new_start:(new_end + 1)])
+      text_span = doc_tokens[new_start:(new_end + 1)]
       if text_span == tok_answer_text:
         return new_start, new_end
 
