aac8e27c7ccc502a2b952529b2637e7bc2a0325a
ecchochan <ecchochan@gmail.com>
2020-09-05 11:42:06 +0800

    update

--- finetune/qa/squad_official_eval_v1.py
+++ finetune/qa/squad_official_eval_v1.py
@@ -101,12 +101,16 @@ def find_lcs(s1, s2):
 	return s1[p-mmax:p], mmax
   
 def f1_score(prediction, ground_truth):
-  lcs, lcs_len = find_lcs(normalize_answer(ground_truth), normalize_answer(prediction))
+  A = normalize_answer(ground_truth)
+  B = normalize_answer(prediction)
+  lcs, lcs_len = find_lcs(A, B)
+
   if lcs_len == 0:
     return 0
   precision 	= 1.0*lcs_len/len(prediction_segs)
   recall 		= 1.0*lcs_len/len(ans_segs)
   f1 			= (2*precision*recall)/(precision+recall)
+  print('%r, %r: %s'%(A, B, f1))
   return f1
 
   prediction_tokens = normalize_answer(prediction).split()
